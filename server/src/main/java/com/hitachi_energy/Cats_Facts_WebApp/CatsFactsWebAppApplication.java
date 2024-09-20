ddf3d494c8ed7c724f078b7778fec038d96d3567
Piotr <trojanpiotrek@interia.pl>
2024-09-20 14:51:12 +0200

    initial commit

---
+++ server/src/main/java/com/hitachi_energy/Cats_Facts_WebApp/CatsFactsWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.hitachi_energy.Cats_Facts_WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsFactsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatsFactsWebAppApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/hitachi_energy/Cats_Facts_WebApp/CatsFactsWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitachi_energy.Cats_Facts_WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsFactsWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
